3c5537e089d60fe2730108c98908d3d9dd6ee7a5
Gergö Barany <gergo@codesourcery.com>
2019-01-11 08:38:29 -0800

    FIXUP: Fix a bug in conditional loop handling: adjust "c-c++-common/goacc/kernels-conditional-execution.c"

--- gcc/testsuite/c-c++-common/goacc/kernels-conditional-execution.c
+++ gcc/testsuite/c-c++-common/goacc/kernels-conditional-execution.c
@@ -16,6 +16,14 @@ kernels_conditionally_executed_loops (void)
 #pragma acc loop gang(num: 3) /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
       for (int i = 0; i < 10; i++)
         ;
+    } else if (count > 7) {
+#pragma acc loop gang(num: 3) /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+      for (int i = 0; i < 10; i++)
+        ;
+    } else {
+#pragma acc loop gang(num: 3) /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+      for (int i = 0; i < 10; i++)
+        ;
     }
 
     while (count-- > 10) {
@@ -77,6 +85,14 @@ forward_jump_label:
 #pragma acc loop gang /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
       for (int i = 0; i < 10; i++)
         ;
+    } else if (count > 7) {
+#pragma acc loop gang /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+      for (int i = 0; i < 10; i++)
+        ;
+    } else {
+#pragma acc loop gang /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+      for (int i = 0; i < 10; i++)
+        ;
     }
 
     while (count-- > 10) {
@@ -123,6 +139,106 @@ backward_jump_label_2:
     for (int i = 0; i < 10; i++)
       ;
 forward_jump_label_2:
+    ;
+  }
+
+  /* The same tests as above, but this time there are two loops in each
+     block instead of just one. */
+#pragma acc kernels num_gangs(3)
+  {
+#pragma acc loop gang
+    for (int i = 0; i < 10; i++)
+      ;
+#pragma acc loop gang
+    for (int k = 0; k < 10; k++)
+      ;
+
+    if (flag) {
+#pragma acc loop gang /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+      for (int i = 0; i < 10; i++)
+        ;
+#pragma acc loop gang /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+      for (int k = 0; k < 10; k++)
+        ;
+    } else if (count > 7) {
+#pragma acc loop gang /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+      for (int i = 0; i < 10; i++)
+        ;
+#pragma acc loop gang /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+      for (int k = 0; k < 10; k++)
+        ;
+    } else {
+#pragma acc loop gang /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+      for (int i = 0; i < 10; i++)
+        ;
+#pragma acc loop gang /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+      for (int k = 0; k < 10; k++)
+        ;
+    }
+
+    while (count-- > 10) {
+#pragma acc loop gang /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+      for (int i = 0; i < 10; i++)
+        ;
+#pragma acc loop gang /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+      for (int k = 0; k < 10; k++)
+        ;
+    }
+
+    do {
+#pragma acc loop gang /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+      for (int i = 0; i < 10; i++)
+        ;
+#pragma acc loop gang /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+      for (int k = 0; k < 10; k++)
+        ;
+    } while (count++ < 100);
+
+    /* This is a non-OpenACC outer loop. */
+    for (int j = 0; j < 10; j++) {
+#pragma acc loop gang /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+      for (int i = 0; i < 10; i++)
+        ;
+#pragma acc loop gang /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+      for (int k = 0; k < 10; k++)
+        ;
+    }
+
+    switch (count)
+    {
+      case 42:
+#pragma acc loop gang /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+      for (int i = 0; i < 10; i++)
+        ;
+#pragma acc loop gang /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+      for (int k = 0; k < 10; k++)
+        ;
+      break;
+
+      default:
+      ;
+    }
+
+backward_jump_label_3:
+    count--;
+#pragma acc loop gang /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+    for (int i = 0; i < 10; i++)
+      ;
+#pragma acc loop gang /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+    for (int k = 0; k < 10; k++)
+      ;
+    if (count == 123)
+      goto backward_jump_label_3;
+
+    if (count == 142)
+      goto forward_jump_label_3;
+#pragma acc loop gang /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+    for (int i = 0; i < 10; i++)
+      ;
+#pragma acc loop gang /* { dg-warning "conditionally executed loop in kernels region will be executed in a single gang; ignoring .gang. clause" } */
+    for (int k = 0; k < 10; k++)
+      ;
+forward_jump_label_3:
     ;
   }
 }
