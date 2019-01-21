26ba8ca93e464e6845d7301b1ed86906423e7b72
Gergö Barany <gergo@codesourcery.com>
2019-01-21 07:16:06 -0800

    Turn OpenACC kernels regions into a sequence of parallel regions
    This patch decomposes each OpenACC kernels region into a sequence of
    parallel regions. Each OpenACC loop nest turns into its own region; any code
    between such loop nests is gathered up into a region as well. The loop
    regions can be distributed across gangs if the original kernels region had a
    num_gangs clause, while the other regions are executed in "gang-single"
    mode. The implied default "auto" clause on kernels loops is made explicit
    unless there is a conflicting clause.
    
        gcc/
        * omp-oacc-kernels.c (top_level_omp_for_in_stmt): New function.
        (make_gang_single_region): Likewise.
        (make_gang_parallel_loop_region): Likewise.
        (flatten_binds): Likewise.
        (make_data_region_try_statement): Likewise.
        (maybe_build_inner_data_region): Likewise.
        (decompose_kernels_region_body): Likewise.
        (transform_kernels_region): Delegate to decompose_kernels_region_body
        and make_data_region_try_statement.
    
        gcc/testsuite/
        * c-c++-common/goacc/kernels-conversion.c: Test for a gang-single
        region.
        * gfortran.dg/goacc/kernels-conversion.f95: Likewise.

--- gcc/testsuite/gfortran.dg/goacc/kernels-conversion.f95
+++ gcc/testsuite/gfortran.dg/goacc/kernels-conversion.f95
@@ -16,6 +16,7 @@ program main
   end do
 
   sum = sum + 1
+  a(1) = a(1) + 1
 
   !$acc loop
   do i = 1, N
@@ -25,10 +26,14 @@ program main
   !$acc end kernels
 end program main
 
-! Check that the kernels region is split into a data region and an enclosed
-! parallel region.
+! Check that the kernels region is split into a data region and enclosed
+! parallel regions.
 ! { dg-final { scan-tree-dump-times "oacc_data" 1 "convert_oacc_kernels" } }
-! { dg-final { scan-tree-dump-times "oacc_parallel" 1 "convert_oacc_kernels" } }
+
+! The two loop regions are parallelized, the sequential part in between is
+! made gang-single.
+! { dg-final { scan-tree-dump-times "oacc_parallel_kernels_parallelized" 2 "convert_oacc_kernels" } }
+! { dg-final { scan-tree-dump-times "oacc_parallel_kernels_gang_single" 1 "convert_oacc_kernels" } }
 
 ! Check that the original kernels region is removed.
 ! { dg-final { scan-tree-dump-not "oacc_kernels" "convert_oacc_kernels" } }
