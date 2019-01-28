85a8970208c2883bc9a870c1be323adc68611bcc
Thomas Schwinge <thomas@codesourcery.com>
2019-01-28 16:02:29 +0100

    into [WIP] New "decompose" algorithm: test cases
    Adjust to 'Revert "[WIP] Support 'dg-message' in libgomp test cases"'.

--- libgomp/testsuite/libgomp.oacc-c-c++-common/kernels-decompose-1.c
+++ libgomp/testsuite/libgomp.oacc-c-c++-common/kernels-decompose-1.c
@@ -11,14 +11,14 @@ int main()
 
 #pragma acc kernels
   {
-    int c = 234; /* { dg-message "note: beginning .gang-single. region in OpenACC .kernels. construct" } */
+    int c = 234; /* { dg-warning "note: beginning .gang-single. region in OpenACC .kernels. construct" } */
 
-#pragma acc loop independent gang /* { dg-message "note: assigned OpenACC gang loop parallelism" } */
-    /* { dg-message "note: parallelized loop nest in OpenACC .kernels. construct" "" { target *-*-* } .-1 } */
+#pragma acc loop independent gang /* { dg-warning "note: assigned OpenACC gang loop parallelism" } */
+    /* { dg-warning "note: parallelized loop nest in OpenACC .kernels. construct" "" { target *-*-* } 16 } */
     for (int i = 0; i < N; ++i)
       b[i] = c;
 
-    a = c; /* { dg-message "note: beginning .gang-single. region in OpenACC .kernels. construct" } */
+    a = c; /* { dg-warning "note: beginning .gang-single. region in OpenACC .kernels. construct" } */
   }
 
   for (int i = 0; i < N; ++i)
